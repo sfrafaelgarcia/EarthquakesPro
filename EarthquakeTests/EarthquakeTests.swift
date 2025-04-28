9e944fae16b75a3ab8f29f1ada3a86d5dc1a5090
rafael garcia <sfrafaelgarcia@gmail.com>
2025-04-28 11:43:58 -0700

    Added tests and sample test data to test the decoding process

--- EarthquakeTests/EarthquakeTests.swift
+++ EarthquakeTests/EarthquakeTests.swift
@@ -5,13 +5,22 @@
 //  Created by Rafael Garcia on 4/27/25.
 //
 
+import Foundation
 import Testing
 @testable import Earthquake
 
 struct EarthquakeTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @Test
+    func geoJSONDecoderDecodesQuake() throws {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .millisecondsSince1970
+        let quake = try decoder.decode(Quake.self, from: testFeature_nc73649170)
+        #expect(quake.code == "73649170")
+        
+        let expectedSeconds = TimeInterval(1636129710550 / 1000.0)
+        let expectedTime = Date(timeIntervalSince1970: expectedSeconds)
+        #expect(quake.time == expectedTime)
     }
 
 }
