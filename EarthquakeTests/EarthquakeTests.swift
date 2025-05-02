9d0d7a6b1fe95f293597fae3c196b467c1c5a5c1
rafael garcia <sfrafaelgarcia@gmail.com>
2025-05-01 20:04:25 -0700

    Decode and test GeoJSON

--- EarthquakeTests/EarthquakeTests.swift
+++ EarthquakeTests/EarthquakeTests.swift
@@ -11,8 +11,7 @@ import Testing
 
 struct EarthquakeTests {
 
-    @Test
-    func geoJSONDecoderDecodesQuake() throws {
+    @Test func geoJSONDecoderDecodesQuake() throws {
         let decoder = JSONDecoder()
         decoder.dateDecodingStrategy = .millisecondsSince1970
         let quake = try decoder.decode(Quake.self, from: testFeature_nc73649170)
@@ -21,6 +20,18 @@ struct EarthquakeTests {
         let expectedSeconds = TimeInterval(1636129710550 / 1000.0)
         let expectedTime = Date(timeIntervalSince1970: expectedSeconds)
         #expect(quake.time == expectedTime)
+    }
+    
+    @Test func geoJSONDecoderDecodesGeoJSON() throws {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .millisecondsSince1970
+        let decoded = try decoder.decode(GeoJSON.self, from: testQuakesData)
+        #expect(decoded.quakes.count == 6)
+        #expect(decoded.quakes[0].code == "73649170")
+
+        let expectedSeconds = TimeInterval(1636129710550 / 1000.0)
+        let expectedTime = Date(timeIntervalSince1970: expectedSeconds)
+        #expect(decoded.quakes[0].time == expectedTime)
     }
 
 }
