04efc8e0156841bba4ff922dc04f9f1e21d56715
rgarciasf <rafael.innova@icloud.com>
2025-05-05 13:35:22 -0700

    added search view

---
+++ Earthquake/Views/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  Earthquake
+//
+//  Created by Rafael Garcia on 5/5/25.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
