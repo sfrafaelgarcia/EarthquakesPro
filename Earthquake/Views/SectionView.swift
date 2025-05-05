9b4b7ac0f4359ad7ca339067c5f0133f87aed218
rgarciasf <rafael.innova@icloud.com>
2025-05-05 12:57:45 -0700

    added section view file

---
+++ Earthquake/Views/SectionView.swift
@@ -0,0 +1,18 @@
+//
+//  SectionView.swift
+//  Earthquake
+//
+//  Created by Rafael Garcia on 5/5/25.
+//
+
+import SwiftUI
+
+struct SectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SectionView()
+}
